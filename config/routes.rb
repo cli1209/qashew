fdf4a415c376fc6a4266d74330e5316b5adac5ac
Casey Li <caseyli12.09@gmail.com>
2017-10-15 18:30:35 -0400

    Add Devise gem
    integrated gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :users
 	root to: 'questions#index'
-  resources :questions
+	resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
