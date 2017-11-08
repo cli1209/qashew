e73b9e5180b1fcacffcc2271b31c85f6172fe2f1
Casey Li <caseyli12.09@gmail.com>
2017-11-08 02:22:19 -0500

    Semi-functional:
    -added caching from acts_as_votable so that questions could be sorted
    -sort questions by their weighted score, updates in real time

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,13 +5,13 @@ class Question < ApplicationRecord
 	validates :user_id, presence: true
 	validates :headline, presence: true, length: { maximum: 150 }
 	validates :content, presence: true
-	default_scope -> {order(created_at: :desc)}
+	default_scope -> {order(:cached_weighted_score => :desc)}
 
 	def self.search(term)
 	  if term
 	    where("headline ILIKE ? OR content ILIKE ?", "%#{term}%", "%#{term}%").order('id DESC')
 	  else
-	    order('id DESC')
+	    order(:cached_weighted_score => :desc)
 	  end
 	end
 end
