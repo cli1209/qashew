5f9393822f460876cefe948f09cdc406f85273e1
Casey Li <caseyli12.09@gmail.com>
2017-12-05 21:07:42 -0500

    make username not case-sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,11 @@ class User < ApplicationRecord
     validates_format_of :email, with: /\@princeton\.edu/, message: 'must be your university email address.'
     validates_uniqueness_of   :email     
     validates_presence_of     :username # required
-    validates_uniqueness_of   :username # required    
+    validates :username,
+      :presence => true,
+      :uniqueness => {
+        :case_sensitive => false
+      }
     validates_presence_of     :password # recommended
     validates_confirmation_of :password # recommended
     validates_length_of       :password, minimum: 6 # recommended
