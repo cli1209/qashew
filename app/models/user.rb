299cf5d943873df92e1abf9fa393bb1cd3ae033e
Casey Li <caseyli12.09@gmail.com>
2017-12-05 21:16:31 -0500

    remove username, replace it with netid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     acts_as_voter
     has_many :questions
     has_many :answers
+    after_create :set_username
+
 
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -11,12 +13,6 @@ class User < ApplicationRecord
     validates_presence_of     :email # optional
     validates_format_of :email, with: /\@princeton\.edu/, message: 'must be your university email address.'
     validates_uniqueness_of   :email     
-    validates_presence_of     :username # required
-    validates :username,
-      :presence => true,
-      :uniqueness => {
-        :case_sensitive => false
-      }
     validates_presence_of     :password # recommended
     validates_confirmation_of :password # recommended
     validates_length_of       :password, minimum: 6 # recommended
@@ -26,5 +22,10 @@ class User < ApplicationRecord
     end
     def owns_answer?(answer)
         id == answer.user_id
+    end
+
+    private
+    def set_username
+       self.username = self.email[/^[^@]+/]
     end
 end
