0134102b0db2489e4b14bdc793180b948c4a84b0
Casey Li <caseyli12.09@gmail.com>
2017-12-23 00:46:05 -0500

    assets pipeline fixes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Qashew
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # make sure assets showing to heroku
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
