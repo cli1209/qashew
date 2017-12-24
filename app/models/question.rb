fd923ab5eb0f47fa62cd6004285f0aa04f2c6683
Casey Li <caseyli12.09@gmail.com>
2017-12-24 00:04:13 -0500

    tag list only contains 3-letter dpt abbreviations

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,6 +8,13 @@ class Question < ApplicationRecord
 	validates :headline, presence: true, length: { maximum: 150 }
 	validates :content, presence: true
 
+	validate :tag_size 
+	def tag_size
+	 self.tag_list.each do |tag|
+	  errors[:tag_list] << "must only contain 3-letter departmental abbreviations" if tag.length != 3 
+	 end
+	end
+
 	def self.tag_search(tag_id)
 		if tag_id
 			Question.tagged_with(tag_id)
