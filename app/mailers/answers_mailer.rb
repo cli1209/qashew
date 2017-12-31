da712727cff53de7784a3222b92f92aecc60ed03
Casey Li <caseyli12.09@gmail.com>
2017-12-31 05:02:02 -0500

    fix emails so that the people who posted the question receive the answer notification, not the askers

--- app/mailers/answers_mailer.rb
+++ app/mailers/answers_mailer.rb
@@ -5,14 +5,14 @@ class AnswersMailer < ApplicationMailer
 	def answer_notification(answer, question, user)
 		@answer = answer
 	    @question = question 
-	    @user = user
+	    @user = @question.user.id
 
         return if @question.user.id == @user.id 
-        mail(to: @user.email, subject: 'Your question was just answered!')
+        mail(to: @user.email, subject: 'A question was just answered!')
 	    @starrers = User.where("?=ANY(starred)", @question.id)
 	    @starrers.each do |starrers|
 	    	if starrers.id != @user.id and starrers.id != @question.user.id
-		    	mail(to: starrers.email, subject: 'Your question was just answered!')
+		    	mail(to: starrers.email, subject: 'A question was just answered!')
 		    end
 	    end
     end
