425a1b815125672d8dc6a6e8d20d234538e1f0e1
Casey Li <caseyli12.09@gmail.com>
2017-12-31 05:08:49 -0500

    fix emails to the askers

--- app/mailers/answers_mailer.rb
+++ app/mailers/answers_mailer.rb
@@ -5,13 +5,15 @@ class AnswersMailer < ApplicationMailer
 	def answer_notification(answer, question, user)
 		@answer = answer
 	    @question = question 
-	    @user = @question.user.id
+	    @asker = User.find_by_id(question.user_id)
 
-        return if @question.user.id == @user.id 
-        mail(to: @user.email, subject: 'A question was just answered!')
+	    # no notification needed if the asker is the same as the answerer
+        return if @asker.id == user.id 
+        mail(to: @asker.email, subject: 'A question was just answered!')
 	    @starrers = User.where("?=ANY(starred)", @question.id)
 	    @starrers.each do |starrers|
-	    	if starrers.id != @user.id and starrers.id != @question.user.id
+	    	# no notification if the starrer is the same as the answerer or the asker
+	    	if starrers.id != user.id and starrers.id != @asker.id
 		    	mail(to: starrers.email, subject: 'A question was just answered!')
 		    end
 	    end
